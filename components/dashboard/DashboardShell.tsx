8e08408dfeeae4c5869cf1a39d2237f14f44eaac
yeison8a35 <yeison8a35@gmail.com>
2026-06-03 23:33:09 -0500

    ok

--- components/dashboard/DashboardShell.tsx
+++ components/dashboard/DashboardShell.tsx
@@ -79,7 +79,7 @@ export default function DashboardShell({
           },
           {
             label: "Mis procesos",
-            href: "#",
+            href: "/usuario/proceso",
             icon: <FileSearch size={18} />,
           },
           {
